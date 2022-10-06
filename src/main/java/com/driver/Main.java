6a080f3d55993ca4fb3511768a6cab44ae5af17f
mayurW1000 <mayur.wable1000@gmail.com>
2022-10-06 14:29:35 +0530

    code done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,29 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x, int y){
+            int z = x * y;
+            return z;
+        }
+
+        public int product(int x, int y, int z){
+            int ans = x * y * z;
+            return ans;
+        }
+
+        public double product(double x, double y){
+            double ans = x * y;
+            return ans;
+        }
+
+    }
+
+    public static void main(String[]args){
+        Product p = new Product();
+        int ans = p.product(10,5);
+        int ans2 = p.product(10,5,2);
+        double ans3 =p.product(2.5,4.5);
+    }
 }
